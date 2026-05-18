2332b35ae2a73439ebf8fd948f7c209e65ae68b3
Timothy <gamertimka9@gmail.com>
2026-05-18 11:34:30 +0300

    feat: implement database models for user, product, retailer, and cart management

---
+++ services/api/database/session.py
@@ -0,0 +1,15 @@
+﻿from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from services.api.plugins.security.secrets.load_secret import get_secret
+
+
+DATABASE_URL = get_secret('DATABASE_URL')
+
+engine = create_async_engine(DATABASE_URL, echo=get_secret("DEBUG") == "True")
+
+AsyncSessionLocal = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
