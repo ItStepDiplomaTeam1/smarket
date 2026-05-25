ac2ca2424eaece110c925913678f8cbd003a8708
Timothy <gamertimka9@gmail.com>
2026-05-25 14:43:50 +0300

    feat: update Docker configuration to include PostgreSQL service and migration job

--- services/api/migrations/env.py
+++ services/api/migrations/env.py
@@ -1,18 +1,23 @@
 import asyncio
 import os
+import sys
 from logging.config import fileConfig
+from pathlib import Path
 
-from sqlalchemy.ext.asyncio import create_async_engine
 from alembic import context
+from sqlalchemy.ext.asyncio import create_async_engine
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL", ""))
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 from database.services.models import Base
+
 target_metadata = Base.metadata
 
 
