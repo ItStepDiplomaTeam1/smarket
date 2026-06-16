5aff038d842b08ade278a11fb49f18d3345e6d5a
Vladyslav Mironenko <vladmironenko2309@gmail.com>
2026-06-16 23:16:42 +0200

    microservice-reviews

---
+++ services/reviews_service/app/shared/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+from uuid import UUID
+from datetime import datetime
+from typing import Optional
+
+class ReviewCreate(BaseModel):
+    product_id: int
+    rating: int = Field(..., ge=1, le=5, description="Рейтинг від 1 до 5")
+    text: Optional[str] = None
+
+class ReviewResponse(BaseModel):
+    id: UUID
+    product_id: int
+    user_id: UUID
+    user_name: str
+    rating: int
+    text: Optional[str]
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
