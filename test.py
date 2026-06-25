a8e7a1fd24aacae29df6cb5eba79b72f28d6763c
MxmXyn245 <shevamax.ua@gmail.com>
2026-06-25 09:46:02 +0300

    Remove test files

--- test.py
+++
@@ -1,9 +0,0 @@
-﻿import urllib.request, json
-try:
-    req = urllib.request.Request("http://157.180.74.21:8080/api/v1/cart/")
-    req.add_header('Authorization', 'Bearer 1')
-    response = urllib.request.urlopen(req)
-    data = json.loads(response.read().decode())
-    print(json.dumps(data, indent=2))
-except Exception as e:
-    print("Error:", e)
